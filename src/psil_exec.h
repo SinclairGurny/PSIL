9b9264b1d5d2b9711f5d7d55055f9a3a510d8077
SinclairGurny <sinclairg@hotmail.com>
2019-07-09 14:33:47 -0400

    Define and basic variable lookups

--- src/psil_exec.h
+++ src/psil_exec.h
@@ -13,8 +13,61 @@
 
 namespace psil_exec {
 
+  // Forward declarations
+
+  struct stack_t;
+
+  // === Typedefs ======================================================================
+  
   using TE_Type = psil_parser::token_elem_t::TE_Type;
   using token_ptr = std::unique_ptr<psil_parser::token_t>;
+  using stack_ptr = std::unique_ptr<stack_t>;
+
+  enum VarType { BOOL, CHAR, NUM, LIST, PROC, UNKNOWN, ERROR };
+  
+  // ===================================================================================
+  
+  /**
+     Copies token recursively
+  */
+  token_ptr copy_tk( const token_ptr & tk );
+
+  /**
+     Checks type of token
+     Assumes top level token is an expression
+  */
+  VarType check_type( const token_ptr & tk );
+  
+  struct stack_elem_t {
+    stack_elem_t( std::string n, VarType t, const token_ptr & v ) :
+      var_name(n), type(t), value( copy_tk( v ) ), scope_lvl(0) {}
+    stack_elem_t( std::string n, VarType t, const token_ptr & v, size_t sl ) :
+      var_name(n), type(t), value( copy_tk( v ) ), scope_lvl(sl) {}
+
+    std::string var_name;
+    VarType type;
+    token_ptr value;
+    size_t scope_lvl;
+  };
+  
+  struct stack_t {
+    enum ExistsType { NO, GLOBAL, LOCAL };
+
+    stack_t() : current_scope(0) { init(); }
+    
+    void add( std::string n, const token_ptr & v );
+    void init();
+    void push();
+    void pop();
+    ExistsType exists( std::string n );
+    token_ptr get( std::string n, ExistsType e );
+
+    size_t current_scope;
+    std::map<std::string, std::unique_ptr<stack_elem_t> > global_table;
+    std::map<std::string, std::unique_ptr<stack_elem_t> > table;
+  };
+  
+  // ===================================================================================
 
   /**
      Read, Evaluate, Print, Loop
@@ -22,7 +75,7 @@ namespace psil_exec {
 
      @param lang -language to parse input using
      @param input - input to evaluate
-   */
+  */
   void repl( const std::unique_ptr<psil_parser::language_t> & lang, std::string input );
 
   // ================== Exec functions ================================================
@@ -31,35 +84,47 @@ namespace psil_exec {
      Executes the abstract syntax tree given
      if rem is true, then delete that branch
   */
-  void exec( token_ptr & ast, bool& rem );
+  void exec( stack_ptr & s, token_ptr & ast, bool& rem );
 
   /**
      Executes the if expression given
      Assumes the node given is the expression token containing the if
      if rem is true, then delete that branch
   */
-  void exec_if( token_ptr & node, bool& rem );
+  void exec_if( stack_ptr & s, token_ptr & node, bool& rem );
 
   /**
      Executes the cond expression given
      Assumes the node given is the expression token containing the cond
      if rem is true, then delete that branch
   */
-  void exec_cond( token_ptr & node, bool& rem );
+  void exec_cond( stack_ptr & s, token_ptr & node, bool& rem );
 
   /**
      Executes the lambda expression given
      Assume the node given is the expression token containing the lambda
      if rem is true, then delete that branch
   */
-  void exec_lambda( token_ptr & node, bool& rem );
+  void exec_lambda( stack_ptr & s, token_ptr & node, bool& rem );
+
+  /**
+     Executes the application of procedures
+     Assumes the node given is the expression token containing the application
+     if rem is true, then delete that branch. ie application returned void
+  */
+  void exec_app( stack_ptr & s, token_ptr & node, bool& rem );
 
   /**
      Executes the definition given
      Assumes the node given is the definition token
      if rem is true, then delete that branch
   */
-  void exec_def( token_ptr & node  );
+  void exec_def( stack_ptr & s, token_ptr & node  );
+
+  /**
+     Replaces variables with their value
+  */
+  void exec_var( stack_ptr & s, token_ptr & node, bool& rem );
   
 
   // ==============================================================================
@@ -67,7 +132,7 @@ namespace psil_exec {
   /**
      Checks to see if a expression evaluates to true
   */
-  bool is_true( token_ptr & node );
+  bool is_true( stack_ptr & s, token_ptr & node );
 
 
   /**
